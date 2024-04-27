c0ffd69a004b82d09f49d4638e36327bfac510eb
sameh92m <samehsamara92@gmail.com>
2024-04-27 17:08:11 +0800

    rabbitMQ

--- AuctionService/Controllers/AuctionsController.cs
+++ AuctionService/Controllers/AuctionsController.cs
@@ -2,6 +2,9 @@
 using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,31 +16,34 @@ public class AuctionsController : ControllerBase
 {
     private readonly AuctionDbContext _context;
     private readonly IMapper _mapper;
+    private readonly IPublishEndpoint _publishEndpoint;
 
-    public AuctionsController(AuctionDbContext context, IMapper mapper)
-    {
+   public AuctionsController(AuctionDbContext context, IMapper mapper,IPublishEndpoint publishEndpoint)
+   {
         _context = context;
         _mapper = mapper;
-    }
+        _publishEndpoint = publishEndpoint;
+   }
 
     [HttpGet]
-    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(string date)
     {
-        var auctions = await _context.Auctions
-            .Include(x => x.Item)
-            .OrderBy(x => x.Item.Make)
-            .ToListAsync();
+        var query = _context.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
 
-        return _mapper.Map<List<AuctionDto>>(auctions);
+        if (!string.IsNullOrEmpty(date))
+        {
+            query = query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
+        }
+        return await query.ProjectTo<AuctionDto>(_mapper.ConfigurationProvider).ToListAsync();
+
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
     {
-        var auction = await _context.Auctions
-            .Include(x => x.Item)
+        var auction = await _context.Auctions.Include(x => x.Item)
             .FirstOrDefaultAsync(x => x.Id == id);
-        
+
         if (auction == null) return NotFound();
 
         return _mapper.Map<AuctionDto>(auction);
@@ -46,18 +52,23 @@ public class AuctionsController : ControllerBase
     [HttpPost]
     public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
     {
-        var auction = _mapper.Map<Auction>(auctionDto);
         // TODO: add current user as seller
+        var auction = _mapper.Map<Auction>(auctionDto);        
         auction.Seller = "test";
 
         _context.Auctions.Add(auction);
+        var newAuction = _mapper.Map<AuctionDto>(auction);
+
+        await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
 
         var result = await _context.SaveChangesAsync() > 0;
 
-        if (!result) return BadRequest("Could not save changes to the DB");
 
-        return CreatedAtAction(nameof(GetAuctionById), 
-            new {auction.Id}, _mapper.Map<AuctionDto>(auction));
+        if (!result) return BadRequest("Could not save changes to the DB");
+       
+
+        return CreatedAtAction(nameof(GetAuctionById),
+             new { auction.Id }, newAuction);
     }
 
     [HttpPut("{id}")]
@@ -76,6 +87,8 @@ public class AuctionsController : ControllerBase
         auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
         auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
 
+        await _publishEndpoint.Publish(_mapper.Map<AuctionUpdated>(auction));
+
         var result = await _context.SaveChangesAsync() > 0;
 
         if (result) return Ok();
@@ -93,6 +106,7 @@ public class AuctionsController : ControllerBase
         // TODO: check seller == username
 
         _context.Auctions.Remove(auction);
+        await _publishEndpoint.Publish<AuctionDeleted>(new AuctionDeleted { Id = auction.Id.ToString() });
 
         var result = await _context.SaveChangesAsync() > 0;
 
---
+++ SearchService/RequestHelpers/MappingProfiles .cs
@@ -0,0 +1,16 @@
+﻿using Contracts;
+using SearchService.Models;
+using AutoMapper;
+namespace SearchService.RequestHelpers
+{
+    public class MappingProfiles:Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<AuctionCreated, Item>();
+            CreateMap<AuctionUpdated, Item>();
+        }
+       
+
+    }
+}
